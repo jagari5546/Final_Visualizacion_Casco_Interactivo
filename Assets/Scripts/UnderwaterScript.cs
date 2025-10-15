45cfbf593816015f0e6a3d895e5187afcf2acb88
jagari5546 <122942343+jagari5546@users.noreply.github.com>
2025-10-15 03:31:35 -0500

    Changes

---
+++ Assets/Scripts/UnderwaterScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnderwaterScript : MonoBehaviour
+{
+  [Header("Depth Parameters")]
+  [SerializeField] private Transform mainCamera;
+  [SerializeField] private int depth = 0;
+}
